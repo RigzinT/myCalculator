539a7b51ecdd9724c28a288c438e0657ae7b69ad
RigzinT <rigzint@gmail.com>
2023-10-31 18:30:51 +1000

    Feature Z: Division added to calculator

--- myCalculator/Program.cs
+++ myCalculator/Program.cs
@@ -10,8 +10,13 @@ namespace myCalculator
       double sum = fno + sno;
       return sum;
     }
+    static double DivMethod(double fno, double sno)
+    {
+         double div = fno / sno;
+         return div;
+    }
 
-    static void Main(string[] args)
+        static void Main(string[] args)
     {
             double fno, sno, sum;
       Console.Write("Enter first number:");
@@ -22,6 +27,17 @@ namespace myCalculator
 
       sum = AddMethod(fno,sno);
       Console.Write("\nThe Sum is:" + sum);
+
+
+     Console.WriteLine("Division method");
+     Console.Write("Enter first number:");
+     double fno_div = Convert.ToDouble(Console.ReadLine());
+
+     Console.Write("\nEnter second number:");
+     double sno_div = Convert.ToDouble(Console.ReadLine());
+
+     double div = AddMethod(fno_div, sno_div);
+     Console.Write("\nThe Sum is:" + div);
     }
   }
 }
