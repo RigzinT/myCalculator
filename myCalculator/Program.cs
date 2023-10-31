1b676c9f3c9c8434d9544ca33556226f9bb261f4
RigzinT <rigzint@gmail.com>
2023-10-31 17:27:32 +1000

    First application code with default dependencies

---
+++ myCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace myCalculator
+{
+  class Program
+  {
+    static double AddMethod(double fno, double sno)
+    {
+      double sum = fno + sno;
+      return sum;
+    }
+
+    static void Main(string[] args)
+    {
+            double fno, sno, sum;
+      Console.Write("Enter first number:");
+      fno = Convert.ToDouble(Console.ReadLine());
+
+      Console.Write("\nEnter second number:");
+      sno = Convert.ToDouble(Console.ReadLine());
+
+      sum = AddMethod(fno,sno);
+      Console.Write("\nThe Sum is:" + sum);
+    }
+  }
+}
